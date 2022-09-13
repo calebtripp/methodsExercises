c4ce8e7bb6c367e9c505dd513d466d58acb461b6
calebtripp <calebtripp@outlook.com>
2022-09-13 11:48:30 -0400

    exercise2complete

--- methodsExercise2/methodsExercise2/methodsExercise2/Program.cs
+++ methodsExercise2/methodsExercise2/methodsExercise2/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.ComponentModel;
+﻿using methodsExercise2;
+using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
-public static void Main(string[] args)
+namespace MethodsExercise2
 {
-    var name = Console.ReadLine();
-
-    var result = Add(10, 20);
-
+    public class MathOperatorMethods
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Method.Add(1,2));
+            Console.WriteLine(Method.Subtract(1,2)); 
+            Console.WriteLine(Method.Multiply(1,2));
+            Console.WriteLine(Method.Divide(1,2));
+            Console.WriteLine(Method.Modulus(1,2));
+        }
+    }
 }
 
-public static int Add(int num1, int num2)
-{ 
-
-
-
-
-}
