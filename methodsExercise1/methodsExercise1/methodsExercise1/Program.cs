7e3572ca53fb969758dcc2bbadfa50917808efe7
calebtripp <calebtripp@outlook.com>
2022-09-14 20:39:55 -0400

    added class to method one exercise

--- methodsExercise1/methodsExercise1/methodsExercise1/Program.cs
+++ methodsExercise1/methodsExercise1/methodsExercise1/Program.cs
@@ -1,18 +1,6 @@
-﻿Console.WriteLine("What is your name?");
-var name = Console.ReadLine();
+﻿using methodsExercise1;
 
-Console.WriteLine("What is your excitement level about coding?");
-var excitement = Console.ReadLine();
+StoryMethods.Name();
 
-Console.WriteLine("What music do you listen to while coding?");
-var music = Console.ReadLine();
 
-Console.WriteLine("What will you gift to yourself upon landing the development job you desire?");
-var gift = Console.ReadLine();
 
-var story = $"{name}'s excitement level about coding is {excitement}.\n" +
-    $"{name} listens to {music} while coding.\n" +
-    $"When {name} lands their first job coding, {name} will gift themself: {gift}.\n" +
-    $"Go TrueCoders!";
-
-Console.WriteLine(story);
